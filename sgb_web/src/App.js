f4d7b9cf33c1b709a5ff3c5c1f5c17a7ca38ff7c
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-04-18 12:17:25 +0900

    logo component

--- sgb_web/src/App.js
+++ sgb_web/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Logo from './pages/components/Logo';
 
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <Logo/>
+        <p>SGBInside</p>
       </div>
     );
   }
