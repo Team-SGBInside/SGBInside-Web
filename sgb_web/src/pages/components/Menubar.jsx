9ce8be1c74117c13b3f89be81c0693e95908424a
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-05-04 15:01:34 +0900

    deleted underline on menubar

--- sgb_web/src/pages/components/Menubar.jsx
+++ sgb_web/src/pages/components/Menubar.jsx
@@ -6,10 +6,10 @@ const Menubar = () => {
     return (
         <>
         <div class="menu">
-            <Link to = '/greenRecord'><span class="green">창의적 체험활동</span>&nbsp;</Link>
-            <Link to = '/redRecord'><span class="red">세부능력 및 특기사항</span>&nbsp;</Link>
-            <Link to = '/pinkRecord'><span class="pink">수상경력</span>&nbsp;</Link>
-            <Link to = '/blueRecord'><span class="blue">독서활동상황</span>&nbsp;</Link>
+            <Link to = '/greenRecord'style={{ textDecoration: "none" }}><span class="green">창의적 체험활동</span>&nbsp;</Link>
+            <Link to = '/redRecord' style={{ textDecoration: "none" }}><span class="red">세부능력 및 특기사항</span>&nbsp;</Link>
+            <Link to = '/pinkRecord' style={{ textDecoration: "none" }}><span class="pink">수상경력</span>&nbsp;</Link>
+            <Link to = '/blueRecord' style={{ textDecoration: "none" }}><span class="blue">독서활동상황</span>&nbsp;</Link>
         </div>
         </>
     );
