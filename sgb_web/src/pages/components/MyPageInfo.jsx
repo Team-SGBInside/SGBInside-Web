8e816d241e741c9a51be6933da36ec4ce48d677a
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-06-18 22:09:52 +0900

    my page info box

--- sgb_web/src/pages/components/MyPageInfo.jsx
+++ sgb_web/src/pages/components/MyPageInfo.jsx
@@ -5,9 +5,23 @@ import InfoBox from './img/InfoBox.png';
 const MyPageInfo = () => {
     return (
         <>
+        <div className="mypage">
             <div className="infobox">
                 <img src={InfoBox} alt="infobox" width="600" height="200"/>
+                <div className="infobox_content">
+                    <span className="info_big">김눈송</span>님, 원하는 건 뭐든지 이루어질거에요!<br/>
+                    <span classNmae="info_small">풋풋고등학교 3학년/ 19세</span>
+                </div>
+            </div><br/>
+        </div>
+        <br/>
+        <div className="mypage2">
+            <div className="infobox2">
+                <div className="total_inside">
+                    <span className="info_big">13</span>개의 생기부 인사이드
+                </div>
             </div>
+        </div>
         </>
     );
 };
