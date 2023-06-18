408319c35d94a04c0e95a05ea26f5cbd55d4e4a9
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-06-18 22:53:00 +0900

    mypage layout

--- sgb_web/src/pages/components/MyPageInfo.jsx
+++ sgb_web/src/pages/components/MyPageInfo.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './MyPageInfo.css';
 import InfoBox from './img/InfoBox.png';
+import menu_whole from './img/menu_whole.png';
+import menu_green from './img/menu_green.png';
+import menu_red from './img/menu_red.png';
+import menu_pink from './img/menu_pink.png';
+import menu_blue from './img/menu_blue.png';
 
 const MyPageInfo = () => {
     return (
@@ -19,6 +24,26 @@ const MyPageInfo = () => {
             <div className="infobox2">
                 <div className="total_inside">
                     <span className="info_big">13</span>개의 생기부 인사이드
+                </div>
+                <br/>
+                <div className="semester_menu">
+                <div className="semester">
+                    <select className="semester_select">
+                        <option>1-1&nbsp;&nbsp;</option>
+                        <option>1-2&nbsp;&nbsp;</option>
+                        <option>2-1&nbsp;&nbsp;</option>
+                        <option>2-2&nbsp;&nbsp;</option>
+                        <option>3-1&nbsp;&nbsp;</option>
+                        <option>3-2&nbsp;&nbsp;</option>
+                    </select>
+                </div>
+                <div className="menu_btn">
+                    <img src={menu_whole} alt="whole" width="80" height="40"/>&nbsp;
+                    <img src={menu_green} alt="green" width="80" height="40"/>&nbsp;
+                    <img src={menu_red} alt="red" width="80" height="40"/>&nbsp;
+                    <img src={menu_pink} alt="pink" width="80" height="40"/>&nbsp;
+                    <img src={menu_blue} alt="blue" width="80" height="40"/>&nbsp;
+                </div>
                 </div>
             </div>
         </div>
