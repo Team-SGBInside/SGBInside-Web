9377328fdc7827ab0474225309b750063273cc55
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-06-29 14:10:28 +0900

    fixed code (doesn't work)

---
+++ sgb_web/src/pages/Test.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import {useEffect,useState} from 'react';
+
+function Test() {
+
+    const [code, setCode] = useState('')
+    const [file, setFile] = useState()
+    const [name, setName] = useState('')
+
+    const handlecode = (event) => {
+        event.preventDefault();
+        setCode(event.target.value);
+    }
+
+    const handlename = (event) => {
+        event.preventDefault();
+        setName(event.target.value);
+    }
+
+    const onChangeImg = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+
+        if(e.target.files) {
+            const uploadFile = e.target.files -[0]
+            formData.appent('file',uploadFile)
+            setFile(uploadFile)
+            console.log(uploadFile)
+            console.log('====useState===')
+            console.log(file)
+        }
+    }
+
+    const onPrint = () => {
+        console.log(file)
+    }
+
+    const onClickLogin = (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('code', code)
+        formData.append('file', file)
+        formData.append('engName', name)
+        axios({
+            method:'post',
+            url: "~~",
+            data: formData,
+        })
+        .then((result)=>{console.log('요청성공')
+        console.log(result)
+    
+    })
+        .catch((error)=>{console.log('요청실패')
+        console.log(error)
+    })
+};
+
+return (
+    <>
+    <h1>TEST PAGE</h1>
+    언어코드{code}<input type="text" onChange={handlecode}></input>
+    <form>
+        <input type="file" id="profile-upload" accept="image/*" onChange={onChangeImg}/>
+    </form>
+    국가명{name}<input type="text" onChange={handlename}></input>
+    <button type="submit" onClick={onClickLogin}>
+        제출
+    </button>
+    </>
+    )
+}
+
+
+export default Test;
--- sgb_web/src/pages/components/PinkForm.jsx
+++ sgb_web/src/pages/components/PinkForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import PinkModal from './PinkModal';
 import pinkform_bg from './img/pinkform_bg.png';
 import pink_alert from './img/pink_alert.png';
@@ -7,68 +7,100 @@ import './PinkForm.css';
 import axios from "axios";
 
 function PinkForm() {
-        const pinkNameRef = useRef();
-        const pinkTypeRef = useRef();
-        const pinkImageRef = useRef();
-        const pinkGradeRef = useRef();
-        const pinkDateRef = useRef();
-        const pinkSemesterRef = useRef();
-        const pinkRoleRef = useRef();
-        const pinkThoughtRef = useRef();
-        
-        const handleMember = async () => {
-            console.log("clicked");
-            try {
-            const response = await axios.post(
-                "http://3.37.215.18:3000/activity/prize",
-                {     
-                    name: pinkNameRef.current.value,
-                    prize : pinkGradeRef.current.value,
-                    date: pinkDateRef.current.value,
-                    semester: pinkSemesterRef.current.value,
-                    prizeImage : pinkImageRef.current.value,
-                    role: pinkRoleRef.current.value,
-                    thoughts: pinkThoughtRef.current.value,
-                    writerId: "009",
-                    type : pinkTypeRef.current.value,
-                },
-                {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                    },
-                }
-            );
-            console.log(response);
-            window.alert("성공적으로 기록되었습니다."); //성공페이지로 라우팅
-            } catch (error) {
-            console.log(error);
-            window.alert("기록에 실패했습니다. 필수입력란을 전부 기입해주세요."); //실패페이지로 라우팅
+
+        //useState
+        const [name, setName] = useState('')
+        const [type, setType] = useState('')
+        const [file, setFile] = useState()
+        const [grade, setGrade] = useState('');
+        const [date, setDate] = useState('');
+        const [semester, setSemester] = useState('');
+        const [role, setRole] = useState('');
+        const [thought, setThought] = useState('');
+
+        const handlename = (event) => {
+            event.preventDefault();
+            setName(event.target.value);
+        }
+
+        const handletype = (event) => {
+            event.preventDefault();
+            setType(event.target.value);
+        }
+
+        const handlegrade = (event) => {
+            event.preventDefault();
+            setGrade(event.target.value);
+        }
+
+        const handledate = (event) => {
+            event.preventDefault();
+            setDate(event.target.value);
+        }
+
+        const handlesemester = (event) => {
+            event.preventDefault();
+            setSemester(event.target.value);
+        }
+
+        const handlerole = (event) => {
+            event.preventDefault();
+            setRole(event.target.value);
+        }
+
+        const handlethought = (event) => {
+            event.preventDefault();
+            setThought(event.target.value);
+        }
+
+        const onChangeImg = (e) => {
+            e.preventDefault();
+            const formData = new FormData();
+
+            if(e.target.file) {
+                const uploadFile = e.target.file[0]
+                formData.append('file',uploadFile)
+                setFile(uploadFile)
+                console.log(uploadFile)
+                console.log('===useState===')
+                console.log(file)
             }
         }
 
-        const handleSubmit = async(e) => {
-            e.preventDefault();
+        const onClickSubmit  = (event) => {
+            console.log("clicked");
+            event.preventDefault();
             const formData = new FormData();
-            formData.append("photo", file.length && file[0].uploadedFile);
-        
-           await axios({
-              method: "post",
-              url: process.env.REACT_APP_STREAMING_COMMENT_URL, //환경변수
-              data: formData,
-              headers: { "Content-Type": "multipart/form-data", Authorization: localStorage.getItem("access_token") }
-            });
-            setCommentValue("");
-            setFile([]);
-        };
-                
-        const handleUpload = (e) => {
-            e.preventDefault();
-            const file = e.target.file[0];
-            setFiles([...file, { uploadedFile: file }]);
-        }; 
+            formData.append('name', name)
+            formData.append('grade', grade)
+            formData.append('date', date)
+            formData.append('semester', semester)
+            formData.append('file', file)
+            formData.append('role', role)
+            formData.append('thought', thought)
+            formData.append('writerId',9)
+            formData.append('type', type)
+
+            axios({
+                method:'post',
+                headers: {"Content-Type": "multipart/form-data",},
+                url: "http://3.37.215.18:3000/activity/prize",
+                data: formData,
+            },
+            )
+            
+            .then(result=>{console.log('요청성공')
+            console.log(result)
+            window.alert('기록성공')
+        })
+            .catch((error)=>{console.log('요청실패')
+            console.log(error)
+            window.alert('기록실패')
+        })
+                    
+    };
 
         return (
-        <form name="file" encType="multipart/form-data" onSubmit={handleSubmit}>  
         <div className="pinkform">
             <div className="pinkform_bg">
             <img src={pinkform_bg} alt="pinkform_bg" width="1200" height="1300"/>
@@ -84,14 +116,14 @@ function PinkForm() {
                             className = "pink_name_input"
                             type="text"
                             placeholder="ex) 정보통신대회 (정보검색부문)"
-                            ref={pinkNameRef}                        
+                            onChange={handlename}                       
                         />
                         <br/>
                         {/* 수상유형 */}    
                         <select
                             className="pink_type_select"
                             name= "pinkType"
-                            ref = {pinkTypeRef}
+                            onChange={handletype}
                             >
                             <option value = "교과우수상">교과우수상</option>
                             <option value = "표창장">표창장</option>
@@ -102,13 +134,14 @@ function PinkForm() {
                         <div className="pink-b">
                         {/* 수상이미지 업로드 */}
                         <label className="pink-label">여기에 상장 이미지를 첨부해줘! (선택)</label><br/>
-                        <input 
+                        <form>
+                        <input
                             type="file"
-                            name="file"
-                            accept="image/*,audio/*,video/mp4,video/x-m4v,application/pdf,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-powerpoint,application/vnd.openxmlformats-officedocument.presentationml.presentation,.csv"
-                            onChange={handleUpload}
-                            ref = {pinkImageRef} 
+                            accept='image/*'
+                            className= "pink-image" 
+                            onChange={onChangeImg}
                         />
+                        </form>
                         <br/>
                         </div>
                     </div>
@@ -119,7 +152,7 @@ function PinkForm() {
                             className = "pink_grade_input"
                             type="text"
                             placeholder="ex) 우수상 (2위)"
-                            ref={pinkGradeRef}
+                            onChange={handlegrade}
                         />
                         <br/><br/>
                         {/* 수상날짜 */}
@@ -127,13 +160,13 @@ function PinkForm() {
                         <input
                             className = "pink_date_input"
                             type="date"
-                            ref={pinkDateRef}
+                            onChange={handledate}
                         /><br/>
                         {/* 수상학기 */}
                         <select
                             className="pink_semester_select"
                             name= "pinkSemester"
-                            ref={pinkSemesterRef}
+                            onChange={handlesemester}
                             >                           
                             <option value="1-1">1학년 1학기</option>
                             <option value="1-2">1학년 2학기</option>
@@ -148,7 +181,7 @@ function PinkForm() {
                         <textarea
                             className="pink_activity_text"
                             placeholder="ex) 정보검색부문에 참여한 만큼 다양한 논문과 검색포털, 웹서비스 등으르 활발히 활용하였다. 그 중, ‘Chat GPT’를 사용해서 정보검색결과를 빠르게 얻어볼 수 있어 유용했고, 입력한 검색어가 정교할 수록 더욱 정교한 답변을 얻을 수 있었다. "
-                            ref={pinkRoleRef}
+                            onChange={handlerole}
                         >
                         </textarea>
                         <br/>
@@ -158,14 +191,14 @@ function PinkForm() {
                         <textarea
                             className="pink_thought_text"
                             placeholder="ex) 평소 관심있는 정보통신 분야에서 수상을 해서 기쁘고  뿌듯했다. 후에 정보통신 분야에서 어떤 진로를 잡을 수 있을까 고민해보게 되는 계기도 되었다.  "
-                            ref={pinkThoughtRef}
+                            onChange={handlethought}
                         >
                         </textarea>
                         <br/><br/>
                         <div className = "pink_button">
                             <PinkModal/>
                             <div className = "tip_button">
-                                <button type="submit" style={{backgroundColor: '#FDE2E6'}} onClick={handleMember}>
+                                <button type="submit" style={{backgroundColor: '#FDE2E6'}} onClick={onClickSubmit}>
                                     <img src={pink_save} alt="pink_save" width="230" height="60"/>
                                 </button>
                             </div>
@@ -173,7 +206,6 @@ function PinkForm() {
                 </div>
             </div>  
         </div>
-        </form>  
         );
     };
 
