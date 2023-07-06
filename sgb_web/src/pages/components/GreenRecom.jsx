1d1c6536b9f5753cebdc61b300233b653af3e0f3
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-07-06 19:17:36 +0900

    added message on green page

--- sgb_web/src/pages/components/GreenRecom.jsx
+++ sgb_web/src/pages/components/GreenRecom.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import greenform_bg from './img/greenform_bg.png';
-
+import green_talk from './img/green_talk.png';
+import './GreenRecom.css';
 
 function GreenRecom () {
     return (
         <div className="greenrecom"> 
-            <div className="greenform_bg">
+            <div className="greenrecom_bg">
                 <img src={greenform_bg} alt="greenform_bg" width="1200" height="1310"/>
+                <div className="greenrecom_content">
+                    <div classNmae="green_talk">   
+                        <img src={green_talk} alt="green_talk" width="555" height="90"/> 
+                    </div>     
+                <br/><br/>
+                </div>
             </div>
         </div>
     );
