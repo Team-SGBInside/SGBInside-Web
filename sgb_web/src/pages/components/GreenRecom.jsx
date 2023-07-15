991fae01c31a29084b295c72e0bdbc99e578a482
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-07-15 20:30:57 +0900

    mentor page button

--- sgb_web/src/pages/components/GreenRecom.jsx
+++ sgb_web/src/pages/components/GreenRecom.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import greenform_bg from './img/greenform_bg.png';
 import green_talk from './img/green_talk.png';
+import green_mentor from './img/green_mentor.png';
 import './GreenRecom.css';
+import { Link } from "react-router-dom";
 
 const GreenRecom = () => {
     return (
@@ -9,19 +11,22 @@ const GreenRecom = () => {
         <div className="greenrecom_bg">
             <img src={greenform_bg} alt="greenform_bg" width="1200" height="1310"/>
             <div className="greenrecom_content">
-                <div classNmae="green_talk">   
-                    <img src={green_talk} alt="green_talk" width="555" height="90"/> 
+                <div className="green_talk">   
+                    <img src={green_talk} alt="green_talk" width="555" height="120"/> 
                 </div>     
-            <br/><br/>
-            {/* Search 관련 코드 */}
-            <div className="search_div_green">
-            <input
-                type="search"
-                placeholder="검색어를 입력 하세요..."
-                name="query"
-                className="search_input_green"
-            />
-            </div>
+                <br/><br/>
+                <div className="green_mentor">
+                <Link to ="/greenMentor"><img src={green_mentor} alt="green_mentor" width="300" height="100"/></Link>
+                </div>
+                {/* Search 관련 코드 */}
+                <div className="search_div_green">
+                <input
+                    type="search"
+                    placeholder="검색어를 입력 하세요..."
+                    name="query"
+                    className="search_input_green"
+                />
+                </div>
             </div>
         </div>
     </div>
