94d56217d7dfe0fa2ebc601ed99c92a1180f89a0
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-07-16 23:27:21 +0900

    components layout

--- sgb_web/src/pages/components/GreenMentor.jsx
+++ sgb_web/src/pages/components/GreenMentor.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import greenform_bg from './img/greenform_bg.png';
+import green_mentor_talk from './img/green_mentor_talk.png';
+import './GreenMentor.css';
+import GreenMentorBanner from './GreenMentorBanner';
 
 const GreenMentor = () => {
     return (
-        <div>
-            Hello Mentor
+    <div className="greenmentor">
+        <div className='greenmentor_bg'>
+            <img src={greenform_bg} alt="greenform_bg" width="1200" height="1310"/>
+            <div className="greenmentor_content">
+                <div className="green_mentor_talk">
+                    <img src={green_mentor_talk} alt="green_mentor_talk" width="555" height="135"/> 
+                </div>
+                <br/><br/>
+                <div className="search_div_green">
+                <input
+                    type="search"
+                    placeholder="검색어를 입력 하세요..."
+                    name="query"
+                    className="search_input_green"
+                />
+                </div>
+                <br/><br/><br/><br/><br/><br/>
+                <div className="green_mentor_banner_div">
+                <GreenMentorBanner/>
+                <GreenMentorBanner/>
+                <GreenMentorBanner/>
+                <GreenMentorBanner/>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
