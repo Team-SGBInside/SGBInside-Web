8bdafbee4315ba16ad0c0c5334f045946594ad89
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-07-17 00:18:01 +0900

    components layout =

--- sgb_web/src/pages/components/PinkRecom.jsx
+++ sgb_web/src/pages/components/PinkRecom.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import pinkform_bg from './img/pinkform_bg.png';
 import pink_talk from './img/pink_talk.png';
 import './PinkRecom.css';
+import PinkMentorBanner from './PinkMentorBanner';
 
 function PinkRecom () {
     return (
@@ -13,7 +14,6 @@ function PinkRecom () {
                         <img src={pink_talk} alt="pink_talk" width="555" height="90"/> 
                     </div>     
                 <br/><br/>
-                {/* Search 관련 코드 */}
                 <div className="search_div_pink">
                 <input
                     type="search"
@@ -21,6 +21,13 @@ function PinkRecom () {
                     name="query"
                     className="search_input_pink"
                 />
+                </div>
+                <br/>
+                <div className="pink_mentor_banner_div">
+                <PinkMentorBanner/>
+                <PinkMentorBanner/>
+                <PinkMentorBanner/>
+                <PinkMentorBanner/>
                 </div>
                 </div>
             </div>
