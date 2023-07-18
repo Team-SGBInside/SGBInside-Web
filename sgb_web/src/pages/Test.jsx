87aced7c2901ca10f330fe7709a7c74da62d8a12
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-07-18 16:05:16 +0900

    msg fixes

--- sgb_web/src/pages/Test.jsx
+++ sgb_web/src/pages/Test.jsx
@@ -55,6 +55,7 @@ const handleInfo = async () => {
         <div>
             <h1> TEST PAGE입니다! </h1>
             <button onClick={handleInfo}>Info Get Button</button>
+            <p>{handleInfo.content}</p>
         </div>
     );
 };
