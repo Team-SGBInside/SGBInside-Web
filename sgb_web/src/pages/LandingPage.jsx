922b5006639bea8600b0b22d556554caaa7e8440
biiit4894 <ghiesf@naver.com>
2023-08-06 15:32:43 +0900

    [FIX] React does not recognize the  prop on a DOM element 에러 해결

--- sgb_web/src/pages/LandingPage.jsx
+++ sgb_web/src/pages/LandingPage.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import './LandingPage.css';
-import landing_student from './components/img/landing_student.png'
-import landing_mentor from './components/img/landing_mentor.png'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./LandingPage.css";
+import landing_student from "./components/img/landing_student.png";
+import landing_mentor from "./components/img/landing_mentor.png";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-    return (
-        <body className="landing-body">
-        <div className="landing-content">
-            <h3 className="landing-light">너의 길을 오롯이 응원해,</h3>
-            <h1 className="landing-bold">생기부 인사이드</h1>
-            <div classNmae="landing-btn">
-            <Link to = "/home"><img src={landing_student} alt="student" width="400" height="100"/>&nbsp;&nbsp;</Link>
-            <Link to = "/mentorHome"><img src={landing_mentor} alt="mentor" width="400" height="100"/></Link>
-            </div>
+  return (
+    <body className="landing-body">
+      <div className="landing-content">
+        <h3 className="landing-light">너의 길을 오롯이 응원해,</h3>
+        <h1 className="landing-bold">생기부 인사이드</h1>
+        <div className="landing-btn">
+          <Link to="/home">
+            <img src={landing_student} alt="student" width="400" height="100" />
+            &nbsp;&nbsp;
+          </Link>
+          <Link to="/mentorHome">
+            <img src={landing_mentor} alt="mentor" width="400" height="100" />
+          </Link>
         </div>
-        <br/>
-        </body>
-    );
+      </div>
+      <br />
+    </body>
+  );
 };
 
 export default LandingPage;
--- sgb_web/src/pages/components/BlueRecom.jsx
+++ sgb_web/src/pages/components/BlueRecom.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import blueform_bg from './img/blueform_bg.png';
-import blue_talk from './img/blue_talk.png';
-import './BlueRecom.css';
+import React from "react";
+import blueform_bg from "./img/blueform_bg.png";
+import blue_talk from "./img/blue_talk.png";
+import "./BlueRecom.css";
 
-function BlueRecom () {
-    return (
-        <div className="bluerecom"> 
-            <div className="bluerecom_bg">
-                <img src={blueform_bg} alt="blueform_bg" width="1200" height="1310"/>
-                <div className="bluerecom_content">
-                    <div classNmae="blue_talk">
-                        <img src={blue_talk} alt="blue_talk" width="555" height="120"/> 
-                    </div>     
-                <br/><br/>
-                {/* Search 관련 코드 */}
-                <div className="search_div_blue">
-                <input
-                    type="search"
-                    placeholder="검색어를 입력 하세요..."
-                    name="query"
-                    className="search_input_blue"
-                />
-                </div>
-                </div>
-            </div>
+function BlueRecom() {
+  return (
+    <div className="bluerecom">
+      <div className="bluerecom_bg">
+        <img src={blueform_bg} alt="blueform_bg" width="1200" height="1310" />
+        <div className="bluerecom_content">
+          <div className="blue_talk">
+            <img src={blue_talk} alt="blue_talk" width="555" height="120" />
+          </div>
+          <br />
+          <br />
+          {/* Search 관련 코드 */}
+          <div className="search_div_blue">
+            <input
+              type="search"
+              placeholder="검색어를 입력 하세요..."
+              name="query"
+              className="search_input_blue"
+            />
+          </div>
         </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
 
 export default BlueRecom;
