7b89484317ecddcc55a4ef14d36e0e991d9a2dae
biiit4894 <ghiesf@naver.com>
2023-08-06 15:32:43 +0900

    [FIX] React does not recognize the  prop on a DOM element 에러 해결

--- sgb_web/src/pages/components/PinkRecom.jsx
+++ sgb_web/src/pages/components/PinkRecom.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import pinkform_bg from './img/pinkform_bg.png';
-import pink_talk from './img/pink_talk.png';
-import './PinkRecom.css';
-import PinkMentorBanner from './PinkMentorBanner';
+import React from "react";
+import pinkform_bg from "./img/pinkform_bg.png";
+import pink_talk from "./img/pink_talk.png";
+import "./PinkRecom.css";
+import PinkMentorBanner from "./PinkMentorBanner";
 
-function PinkRecom () {
-    return (
-        <div className="pinkrecom"> 
-            <div className="pinkrecom_bg">
-                <img src={pinkform_bg} alt="pinkform_bg" width="1200" height="1310"/>
-                <div className="pinkrecom_content">
-                    <div classNmae="pink_talk">   
-                        <img src={pink_talk} alt="pink_talk" width="555" height="130"/> 
-                    </div>     
-                <br/><br/>
-                <div className="search_div_pink">
-                <input
-                    type="search"
-                    placeholder="검색어를 입력 하세요..."
-                    name="query"
-                    className="search_input_pink"
-                />
-                </div>
-                <br/>
-                <div className="pink_mentor_banner_div">
-                <PinkMentorBanner/>
-                <PinkMentorBanner/>
-                <PinkMentorBanner/>
-                <PinkMentorBanner/>
-                </div>
-                </div>
-            </div>
+function PinkRecom() {
+  return (
+    <div className="pinkrecom">
+      <div className="pinkrecom_bg">
+        <img src={pinkform_bg} alt="pinkform_bg" width="1200" height="1310" />
+        <div className="pinkrecom_content">
+          <div className="pink_talk">
+            <img src={pink_talk} alt="pink_talk" width="555" height="130" />
+          </div>
+          <br />
+          <br />
+          <div className="search_div_pink">
+            <input
+              type="search"
+              placeholder="검색어를 입력 하세요..."
+              name="query"
+              className="search_input_pink"
+            />
+          </div>
+          <br />
+          <div className="pink_mentor_banner_div">
+            <PinkMentorBanner />
+            <PinkMentorBanner />
+            <PinkMentorBanner />
+            <PinkMentorBanner />
+          </div>
         </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
 
 export default PinkRecom;
