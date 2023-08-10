253a7d3a3c5edc226bbd19082e7fb28e3f08ee28
biiit4894 <ghiesf@naver.com>
2023-08-11 00:31:00 +0900

    [FEAT] 대학생 멘토 창의적 체험활동 개별 조회 api 연동

--- sgb_web/src/pages/components/GreenMentorBanner.jsx
+++ sgb_web/src/pages/components/GreenMentorBanner.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import green_banner_bg from "./img/green_banner_bg.png";
 import "./GreenMentorBanner.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { getCookie, setCookie } from "../../lib/cookie";
 
 // props로 전달된 key들의 value값들을 활용
 const GreenMentorBanner = ({
+  activityId,
   activityType,
   name,
   writerSchoolMajor,
   writerGrade,
 }) => {
+  const navigator = useNavigate();
+
+  console.log("activityId: ", activityId);
   console.log("activityType: ", activityType);
   console.log("name: ", name);
   console.log("writerSchoolMajor: ", writerSchoolMajor);
   console.log("writerGrade: ", writerGrade);
 
+  const handleClick = () => {
+    console.log("activity clicked");
+    navigator(`/greenMentor/${activityId}`);
+  };
   return (
     <>
       <div className="green_mentor_banner">
         <div className="green_banner_bg">
           <img
             src={green_banner_bg}
+            onClick={handleClick}
             alt="green_banner_bg"
             width="200"
             height="100"
           />
           <div className="green_banner_content">
-            <div className="activity_type">{activityType}</div>
-            <div className="activity_name">{name}</div>
-            <div className="activity_writer">
+            <div className="activity_type" onClick={handleClick}>
+              {activityType}
+            </div>
+            <div className="activity_name" onClick={handleClick}>
+              {name}
+            </div>
+            <div className="activity_writer" onClick={handleClick}>
               {writerSchoolMajor} {writerGrade}학년
             </div>
           </div>
