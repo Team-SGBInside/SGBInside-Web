37e91b480a9af3ad11d73ea935c5654130c1730f
biiit4894 <ghiesf@naver.com>
2023-08-10 22:42:39 +0900

    [FEAT] 대학생 멘토 창의적 체험활동 전체 조회 api 연동

--- sgb_web/src/pages/components/GreenMentorBanner.jsx
+++ sgb_web/src/pages/components/GreenMentorBanner.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import green_banner_bg from './img/green_banner_bg.png';
-import './GreenMentorBanner.css';
+import React from "react";
+import green_banner_bg from "./img/green_banner_bg.png";
+import "./GreenMentorBanner.css";
 
-const GreenMentorBanner = () => {
-    return (
-        <div className="green_mentor_banner">
+// props로 전달된 key들의 value값들을 활용
+const GreenMentorBanner = ({
+  activityType,
+  name,
+  writerSchoolMajor,
+  writerGrade,
+}) => {
+  console.log("activityType: ", activityType);
+  console.log("name: ", name);
+  console.log("writerSchoolMajor: ", writerSchoolMajor);
+  console.log("writerGrade: ", writerGrade);
+
+  return (
+    <>
+      <div className="green_mentor_banner">
         <div className="green_banner_bg">
-            <img src={green_banner_bg} alt="green_banner_bg" width="200" height="100"/>
-            <div className="green_banner_content">
-                <div className="activity_type">활동유형</div>
-                <div className="activity_name">활동제목 SAMPLE</div>
-                <div className="activity_writer">글쓴이 SAMPLE</div>
+          <img
+            src={green_banner_bg}
+            alt="green_banner_bg"
+            width="200"
+            height="100"
+          />
+          <div className="green_banner_content">
+            <div className="activity_type">{activityType}</div>
+            <div className="activity_name">{name}</div>
+            <div className="activity_writer">
+              {writerSchoolMajor} {writerGrade}학년
             </div>
+          </div>
         </div>
-        </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default GreenMentorBanner;
