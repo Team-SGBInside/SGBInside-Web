97a05daaadcef106736eb43c6a63b034b2f8236b
biiit4894 <ghiesf@naver.com>
2023-08-11 07:11:00 +0900

    [FEAT] 대학생 멘토 수상경력 개별 조회 api 연동

--- sgb_web/src/pages/components/PinkMentorBanner.jsx
+++ sgb_web/src/pages/components/PinkMentorBanner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import pink_banner_bg from "./img/pink_banner_bg.png";
 import "./PinkMentorBanner.css";
 
@@ -8,20 +8,34 @@ const PinkMentorBanner = ({
   type,
   writerSchoolMajor,
   writerGrade,
+  parentFunction,
 }) => {
+  const [data, setData] = useState(true);
+
+  const handleClick = () => {
+    console.log("activity clicked");
+    parentFunction(data, activityId);
+    console.log("activity clicked 2");
+  };
+
   return (
     <div className="pink_mentor_banner">
       <div className="pink_banner_bg">
         <img
           src={pink_banner_bg}
+          onClick={handleClick}
           alt="pink_banner_bg"
           width="200"
           height="100"
         />
         <div className="pink_banner_content">
-          <div className="activity_type">{type}</div>
-          <div className="activity_name_pink">{name}</div>
-          <div className="activity_writer">
+          <div className="activity_type" onClick={handleClick}>
+            {type}
+          </div>
+          <div className="activity_name_pink" onClick={handleClick}>
+            {name}
+          </div>
+          <div className="activity_writer" onClick={handleClick}>
             {writerSchoolMajor} {writerGrade}학년
           </div>
         </div>
