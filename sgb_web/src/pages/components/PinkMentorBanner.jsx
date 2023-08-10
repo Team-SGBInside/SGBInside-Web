f7d8d7b0b154dd6140181fa995de12e3b6d7eb0e
biiit4894 <ghiesf@naver.com>
2023-08-11 05:47:47 +0900

    [FEAT] 선배 수상경력 전체조회 api 연동

--- sgb_web/src/pages/components/PinkMentorBanner.jsx
+++ sgb_web/src/pages/components/PinkMentorBanner.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import pink_banner_bg from './img/pink_banner_bg.png';
-import './PinkMentorBanner.css';
+import React from "react";
+import pink_banner_bg from "./img/pink_banner_bg.png";
+import "./PinkMentorBanner.css";
 
-const PinkMentorBanner = () => {
-    return (
-        <div className="pink_mentor_banner">
-        <div className="pink_banner_bg">
-            <img src={pink_banner_bg} alt="pink_banner_bg" width="200" height="100"/>
-            <div className="pink_banner_content">
-                <div className="activity_type">대회유형</div>
-                <div className="activity_name">대회제목 SAMPLE</div>
-                <div className="activity_writer">글쓴이 SAMPLE</div>
-            </div>
+const PinkMentorBanner = ({
+  activityId,
+  name,
+  type,
+  writerSchoolMajor,
+  writerGrade,
+}) => {
+  return (
+    <div className="pink_mentor_banner">
+      <div className="pink_banner_bg">
+        <img
+          src={pink_banner_bg}
+          alt="pink_banner_bg"
+          width="200"
+          height="100"
+        />
+        <div className="pink_banner_content">
+          <div className="activity_type">{type}</div>
+          <div className="activity_name_pink">{name}</div>
+          <div className="activity_writer">
+            {writerSchoolMajor} {writerGrade}학년
+          </div>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PinkMentorBanner;
