e251058d9822ff2df19bcef099fd67f845020d0c
biiit4894 <ghiesf@naver.com>
2023-08-10 22:22:50 +0900

    [FEAT] 학과별 대학생 멘토 창의적 체험활동 전체 조회 api 연동

--- sgb_web/src/pages/components/GreenMentor.jsx
+++ sgb_web/src/pages/components/GreenMentor.jsx
@@ -1,38 +1,103 @@
-import React from 'react';
-import greenform_bg from './img/greenform_bg.png';
-import green_mentor_talk from './img/green_mentor_talk.png';
-import './GreenMentor.css';
-import GreenMentorBanner from './GreenMentorBanner';
+import React, { useState } from "react";
+import greenform_bg from "./img/greenform_bg.png";
+import green_mentor_talk from "./img/green_mentor_talk.png";
+import "./GreenMentor.css";
+import axios from "axios";
+import GreenMentorBanner from "./GreenMentorBanner";
+import { getCookie, setCookie } from "../../lib/cookie";
 
 const GreenMentor = () => {
-    return (
+  const sort = "all";
+  const [major, setMajor] = useState("");
+  const trimmedMajor = major.split(" ").join(""); // 검색어 공백제거
+  const [activityList, setActivityList] = useState([]);
+
+  const handleMajor = (e) => {
+    e.preventDefault();
+    setMajor(e.target.value);
+  };
+
+  const onKeySubmitSearch = async (e) => {
+    if (e.key === "Enter") {
+      if (!major || trimmedMajor === "") {
+        alert("검색어를 입력해주세요");
+        return;
+      }
+      await axios({
+        method: "post",
+        url: `http://3.37.215.18:3000/recommend/creative?sort=${sort}`,
+        data: {
+          major: major,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getCookie("accessToken")}`,
+        },
+      })
+        .then((result) => {
+          console.log("요청성공");
+          console.log(result);
+          console.log(result.data.data); // 전체 창체 활동 배열
+          setActivityList(result.data.data);
+        })
+        .catch((error) => {
+          alert("멘토 창의적체험활동 조회 실패");
+          console.log("멘토 창의적체험활동 조회 실패");
+          console.log(error);
+        });
+      // await getMajorInfo();
+    }
+  };
+
+  return (
     <div className="greenmentor">
-        <div className='greenmentor_bg'>
-            <img src={greenform_bg} alt="greenform_bg" width="1200" height="1310"/>
-            <div className="greenmentor_content">
-                <div className="green_mentor_talk">
-                    <img src={green_mentor_talk} alt="green_mentor_talk" width="555" height="135"/> 
-                </div>
-                <br/><br/>
-                <div className="search_div_green">
-                <input
-                    type="search"
-                    placeholder="검색어를 입력 하세요..."
-                    name="query"
-                    className="search_input_green"
+      <div className="greenmentor_bg">
+        <img src={greenform_bg} alt="greenform_bg" width="1200" height="1310" />
+        <div className="greenmentor_content">
+          <div className="green_mentor_talk">
+            <img
+              src={green_mentor_talk}
+              alt="green_mentor_talk"
+              width="555"
+              height="135"
+            />
+          </div>
+          <br />
+          <br />
+          <div className="search_div_green">
+            <input
+              type="search"
+              placeholder="검색어를 입력 하세요..."
+              name="query"
+              className="search_input_green"
+              onChange={handleMajor}
+              onKeyPress={onKeySubmitSearch}
+            />
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="green_mentor_banner_div">
+            {/* 꼭 [{}, {}, {}]  이런 배열 내 0번 인덱스에 먼저 접근하고, 내부의 객체 여러개에 접근하기*/}
+            {activityList[0] &&
+              activityList[0].map((item) => (
+                // GMB 컴포넌트의 props로 배열 내 객체들의 key의 value값들을 보냄
+                <GreenMentorBanner
+                  activityId={item.activityId}
+                  name={item.name}
+                  activityType={item.activityType}
+                  writerSchoolMajor={item.writerSchoolMajor}
+                  writerGrade={item.writerGrade}
                 />
-                </div>
-                <br/><br/><br/><br/><br/><br/>
-                <div className="green_mentor_banner_div">
-                <GreenMentorBanner/>
-                <GreenMentorBanner/>
-                <GreenMentorBanner/>
-                <GreenMentorBanner/>
-                </div>
-            </div>
+              ))}
+          </div>
         </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default GreenMentor;
