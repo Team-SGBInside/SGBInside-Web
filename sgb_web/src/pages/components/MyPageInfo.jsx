4ade7eb26a85451673767d88738f52b4a6e4428c
biiit4894 <ghiesf@naver.com>
2023-08-16 02:49:02 +0900

    [FEAT] 고교생 마이페이지 유형별/학기별 전체 활동 조회 (클릭 2회 필요한 상태)

--- sgb_web/src/pages/components/MyPageInfo.jsx
+++ sgb_web/src/pages/components/MyPageInfo.jsx
@@ -7,6 +7,10 @@ import menu_green from "./img/menu_green.png";
 import menu_red from "./img/menu_red.png";
 import menu_pink from "./img/menu_pink.png";
 import menu_blue from "./img/menu_blue.png";
+import green_clicked from "./img/green_clicked.png";
+import red_clicked from "./img/red_clicked.png";
+import pink_clicked from "./img/pink_clicked.png";
+import blue_clicked from "./img/blue_clicked.png";
 import mypage_green from "./img/mypage_green.png";
 import mypage_red from "./img/mypage_red.png";
 import mypage_pink from "./img/mypage_pink.png";
@@ -52,6 +56,8 @@ const MyPageInfo = () => {
   };
 
   const getUserInfo = (sortQuery, semesterQuery) => {
+    console.log("semesterQuery in getUserInfo: ", semesterQuery);
+    console.log("sortQuery in getUserInfo: ", sortQuery);
     axios
       .get(
         `http://3.37.215.18:3000/mypage?sort=${sortQuery}&semester=${semesterQuery}`,
@@ -72,16 +78,15 @@ const MyPageInfo = () => {
         const name = result.name;
         const userId = result.userId;
         const totalActivitycount = result.totalActivity.activityCount;
+        console.log("totalActivityCount: ", totalActivitycount);
+        console.log("response:", response);
+        console.log("response.data: ", response.data);
+        console.log("response.data.data: ", response.data.data);
 
         // 활동 유형별 배열 내부 객체에 활동 유형 구별가능하도록 sort key 추가
-        let allBookActivity = result.totalActivity.allBookActivity;
-        allBookActivity.map((item) => {
-          item.sort = "book";
-        });
-
-        let allPrizeActivity = result.totalActivity.allPrizeActivity;
-        allPrizeActivity.map((item) => {
-          item.sort = "prize";
+        let allCreativeActivity = result.totalActivity.allcreativeActivity;
+        allCreativeActivity.map((item) => {
+          item.sort = "creative";
         });
 
         let allSubjectDetailedActivity =
@@ -90,9 +95,16 @@ const MyPageInfo = () => {
           item.sort = "subject";
         });
 
-        let allCreativeActivity = result.totalActivity.allcreativeActivity;
-        allCreativeActivity.map((item) => {
-          item.sort = "creative";
+        let allPrizeActivity = result.totalActivity.allPrizeActivity;
+
+        allPrizeActivity.map((item) => {
+          item.sort = "prize";
+        });
+
+        let allBookActivity = result.totalActivity.allBookActivity;
+
+        allBookActivity.map((item) => {
+          item.sort = "book";
         });
 
         // 전체 활동 통합, 배열 내 인덱스 번호에 따른 uniqId로 id key 부여
@@ -111,9 +123,6 @@ const MyPageInfo = () => {
         console.log("allActivity: ", allActivity);
         // console.log("newAllActivity: ", newAllActivity);
         // 순차적으로 response를 찍어보는 코드인데, 필요에 따라 취사선택하세요
-        console.log("response:", response);
-        console.log("response.data: ", response.data);
-        console.log("response.data.data: ", response.data.data);
 
         // 객체 내에 객체(totalActivity)가 담기지 않도록 totalAcitivity 안 요소를 꺼내 data로 재구성
         const data = {
@@ -128,6 +137,11 @@ const MyPageInfo = () => {
         };
         console.log("data: ", data);
         setUserInfo(data);
+        if (data.allActivity.length === 0) {
+          // var activityList = document.getElementById("activity_list");
+          // console.log("no data activityList: ", activityList);
+          // activityList.innerText = `아직 해당 조건으로 작성한 활동이 없습니다.`;
+        }
         return data;
       })
       .catch((error) => {
@@ -135,88 +149,7 @@ const MyPageInfo = () => {
       });
   };
   useEffect(() => {
-    axios
-      .get(
-        `http://3.37.215.18:3000/mypage?sort=${sortQuery}&semester=${semesterQuery}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${getCookie("accessToken")}`,
-            withCredentials: true,
-          },
-        }
-      )
-      .then((response) => {
-        console.log("response: ", response);
-        const result = response.data.data;
-        const age = result.age;
-        const grade = result.grade;
-        const school = result.school;
-        const isTeen = result.isTeen;
-        const name = result.name;
-        const userId = result.userId;
-        const totalActivitycount = result.totalActivity.activityCount;
-
-        // 활동 유형별 배열 내부 객체에 활동 유형 구별가능하도록 sort key 추가
-        let allBookActivity = result.totalActivity.allBookActivity;
-        allBookActivity.map((item) => {
-          item.sort = "book";
-        });
-
-        let allPrizeActivity = result.totalActivity.allPrizeActivity;
-        allPrizeActivity.map((item) => {
-          item.sort = "prize";
-        });
-
-        let allSubjectDetailedActivity =
-          result.totalActivity.allSubjectDetailedActivity;
-        allSubjectDetailedActivity.map((item) => {
-          item.sort = "subject";
-        });
-
-        let allCreativeActivity = result.totalActivity.allcreativeActivity;
-        allCreativeActivity.map((item) => {
-          item.sort = "creative";
-        });
-
-        // 전체 활동 통합, 배열 내 인덱스 번호에 따른 uniqId로 id key 부여
-        let allActivity = [
-          ...allBookActivity,
-          ...allPrizeActivity,
-          ...allSubjectDetailedActivity,
-          ...allCreativeActivity,
-        ];
-
-        allActivity.map((item, index) => {
-          let uniqId = parseInt(index);
-          item.id = uniqId;
-        });
-
-        console.log("allActivity: ", allActivity);
-        // console.log("newAllActivity: ", newAllActivity);
-        // 순차적으로 response를 찍어보는 코드인데, 필요에 따라 취사선택하세요
-        console.log("response:", response);
-        console.log("response.data: ", response.data);
-        console.log("response.data.data: ", response.data.data);
-
-        // 객체 내에 객체(totalActivity)가 담기지 않도록 totalAcitivity 안 요소를 꺼내 data로 재구성
-        const data = {
-          age,
-          grade,
-          school,
-          isTeen,
-          name,
-          totalActivitycount,
-          allActivity,
-          userId,
-        };
-        console.log("data: ", data);
-        setUserInfo(data);
-        return data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    getUserInfo(sortQuery, semesterQuery);
   }, []);
 
   const parentFunction = (data, activityId) => {
@@ -233,13 +166,7 @@ const MyPageInfo = () => {
     // closeBtn.style.display = "flex";
     // showCreativeActivityById(activityId);
   };
-  // 학기 및 활동유형 버튼 클릭 시 액션
-  // const handleClick = (x) => {
-  //   useEffect(() => {
-  //     setActivitySort(x);
-  //   }, []);
-  //   console.log("activitySort: ", activitySort);
-  // };
+
   return (
     <>
       <div className="mypage">
@@ -322,7 +249,7 @@ const MyPageInfo = () => {
       </div>
       <br />
       <div className="mypage3">
-        <div className="activity_list">
+        <div className="activity_list" id="activity_list">
           {userInfo.allActivity &&
             userInfo.allActivity.map((item) => {
               if (item.sort === "creative") {
