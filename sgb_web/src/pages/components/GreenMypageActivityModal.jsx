11eaeedf4c5e6ee3d03595074cadd2d9f16a54da
biiit4894 <ghiesf@naver.com>
2023-08-18 05:15:07 +0900

    [ADD] 마이페이지 창체 개별 활동 모달 내부 텍스트 컴포넌트화 (테스트용도)

--- sgb_web/src/pages/components/GreenMypageActivityModal.jsx
+++ sgb_web/src/pages/components/GreenMypageActivityModal.jsx
@@ -2,28 +2,55 @@ import React, { useEffect, useState } from "react";
 
 export default function GreenMypageActivityModal({
   activityId,
+  name,
   startDate,
   endDate,
+  semester,
+  activityType,
+  role,
+  thoughts,
 }) {
   console.log(
-    "activityId, startDate, endDate:",
+    "props:",
     activityId,
+    name,
     startDate,
-    endDate
+    endDate,
+    semester,
+    activityType,
+    role,
+    thoughts
   );
 
-  // 활동 수정을 위한 state
+  // 활동 수정을 위한 state - props와 변수 선언이 겹치면 안됨
   const [text, setText] = useState("Hello World");
+  const [nme, setNme] = useState("활동명");
+  const [stDate, setStDate] = useState("활동 시작일");
+  const [edDate, setEdDate] = useState("활동 종료일");
+  const [smester, setSmester] = useState("활동 학기");
+  const [actType, setActType] = useState("창체 활동 유형");
+  const [rle, setRle] = useState("활동 내 역할과 구체적인 활동 내용");
+  const [thghts, setThghts] = useState("활동 소감");
+
   const [isEditable, setIsEditable] = useState(false);
 
-  const id = activityId;
-  const stDate = startDate;
-  const edDate = endDate;
+  console.log("isEditable: ", isEditable);
 
-  const example = stDate + " ~ " + edDate;
+  /* 창체 전체 필드
+  // modal.innerText = `sort: 창체
+  // ${activity[0].name} | ${activity[0].startDate} ~ ${activity[0].endDate} | ${activity[0].semester} | ${activity[0].activityType}\n
+  // ${activity[0].name}(${activity[0].startDate} ~ ${activity[0].endDate}) ${activity[0].role}\n
+  // ${activity[0].thoughts}
+  // `; */
 
   useEffect(() => {
-    setText(example);
+    setNme(name);
+    setStDate(startDate);
+    setEdDate(endDate);
+    setSmester(semester);
+    setActType(activityType);
+    setRle(role);
+    setThghts(thoughts);
   }, []);
 
   const handleDoubleClick = () => {
@@ -31,7 +58,7 @@ export default function GreenMypageActivityModal({
   };
 
   const handleChange = (e) => {
-    setText(e.target.value);
+    setNme(e.target.value);
   };
 
   const handleKeyDown = (e) => {
@@ -42,18 +69,110 @@ export default function GreenMypageActivityModal({
 
   return (
     <div className="App">
-      <h1>Click-To-Edit UI</h1>
-      <p className="description">Double click the text below to edit.</p>
-      {isEditable ? (
-        <input
-          type="text"
-          value={text}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        />
-      ) : (
-        <p onDoubleClick={handleDoubleClick}>{text}</p>
-      )}
+      <p className="description">
+        Double click the text below to edit.
+        {isEditable ? (
+          <>
+            <br />
+            활동명:{" "}
+            <input
+              type="text"
+              value={nme}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>활동명: {nme}</p>
+        )}
+        {isEditable ? (
+          <>
+            <br />
+            시작일:{" "}
+            <input
+              type="text"
+              value={stDate}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>시작일: {stDate}</p>
+        )}
+        {isEditable ? (
+          <>
+            <br />
+            종료일:{" "}
+            <input
+              type="text"
+              value={edDate}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>종료일: {edDate}</p>
+        )}
+        {isEditable ? (
+          <>
+            <br />
+            학기:{" "}
+            <input
+              type="text"
+              value={smester}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>학기: {smester}</p>
+        )}
+        {isEditable ? (
+          <>
+            <br />
+            활동유형:{" "}
+            <input
+              type="text"
+              value={actType}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>활동유형: {actType}</p>
+        )}
+        {isEditable ? (
+          <>
+            <br />
+            활동 내 역할, 구체적 활동 내용:
+            <input
+              type="text"
+              value={role}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>
+            활동 내 역할, 구체적 활동 내용: <br />
+            {role}
+          </p>
+        )}
+        {isEditable ? (
+          <>
+            <br />
+            활동 소감:
+            <input
+              type="text"
+              value={thoughts}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <p onDoubleClick={handleDoubleClick}>활동 소감: {thoughts}</p>
+        )}
+      </p>
     </div>
   );
 }
