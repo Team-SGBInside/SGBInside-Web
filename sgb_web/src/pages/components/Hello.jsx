c2f8d0acf79617fd9836b44abe1fcc03066d9f0f
biiit4894 <ghiesf@naver.com>
2023-08-20 04:11:48 +0900

    [FEAT] Hello 컴포넌트에 로그인 버튼 추가 + 계정정보 조회 api 통신방식 .then .catch 문으로 변경

--- sgb_web/src/pages/components/Hello.jsx
+++ sgb_web/src/pages/components/Hello.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect, useId } from "react";
 import axios from "axios";
 import "./Hello.css";
 import mypage_btn from "./img/mypage_btn.png";
+import logout from "./img/logout.png";
 import { Link } from "react-router-dom";
-import { getCookie, setCookie } from "../../lib/cookie";
-import { useNavigate } from "react-router-dom";
+import { getCookie, setCookie, removeCookie } from "../../lib/cookie";
 
-const getUserInfo = async () => {
+const Hello = () => {
+  const [userInfo, setUserInfo] = useState({});
+
   const userId = getCookie("userId");
   const token = getCookie("accessToken");
 
@@ -14,52 +16,44 @@ const getUserInfo = async () => {
     console.log("cannnot get userId or token from cookie");
     return;
   }
-  try {
-    // await 없이 getCookie해도 되는듯합니다!
 
-    //* 마이페이지 계정정보 조회 및 전체활동 조회 api의 response data에서 계정정보만 활용하기
-    const sortQuery = "all";
-    const semseterQuery = "all";
-    const response = await axios.get(
-      `http://3.37.215.18:3000/mypage?sort=${sortQuery}&semester=${semseterQuery}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${getCookie("accessToken")}`,
-          withCredentials: true,
-        },
-      }
-    );
-    const result = response.data.data;
-    console.log("Hello result: ", result);
-    const age = result.age;
-    const grade = result.grade;
-    const isTeen = result.isTeen;
-    // 일단 Hello에서는 이름만 받아오면 되는 상태
-    const name = result.name;
-    const totalActivitycount = result.totalActivity.activityCount;
-    // 순차적으로 response를 찍어보는 코드인데, 필요에 따라 취사선택하세요
-    console.log(response);
-    console.log(response.data);
-    console.log(response.data.data);
-    const data = {
-      // age,
-      // grade,
-      // isTeen,
-      name,
-      // totalActivitycount,
-    };
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error);
+  function logOut() {
+    // alert에서 확인을 누를 경우에 쿠키 삭제
+    if (confirm("로그아웃하시겠습니까?") == true) {
+      removeCookie("userId");
+      removeCookie("accessToken");
+      console.log("userId", getCookie("userId"));
+      console.log("accessToken", getCookie("accessToken"));
+      window.location.href = "/home";
+    } else {
+      return;
+    }
   }
-};
-// userInfo 변수에는 getUserInfo의 호출 결과가 담김(async함수이기 때문에 await문 사용)
-// -> return되는 것은 data이고,
-// data 안에는 name이 있음
-const userInfo = await getUserInfo();
-const Hello = () => {
+
+  //* 마이페이지 계정정보 조회 및 전체활동 조회 api의 response data에서 계정정보만 활용하기
+  const sortQuery = "all";
+  const semseterQuery = "all";
+  useEffect(() => {
+    axios
+      .get(
+        `http://3.37.215.18:3000/mypage?sort=${sortQuery}&semester=${semseterQuery}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getCookie("accessToken")}`,
+            withCredentials: true,
+          },
+        }
+      )
+      .then((response) => {
+        setUserInfo(response.data.data);
+        // 여기서 userInfo state값 콘솔을 찍으면 undefined가 나오지만, state는 정상적으로 반영되는 중임.
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <div className="hello">
@@ -67,9 +61,14 @@ const Hello = () => {
         &nbsp;
         <span className="light-text">님!</span>
         &nbsp; <br />
-        <Link to="/mypage">
-          <img src={mypage_btn} alt="my_page" width="85" height="28" />
-        </Link>
+        <div id="mypage-btn-hello">
+          <Link to="/mypage">
+            <img src={mypage_btn} alt="my_page" width="85" height="28" />
+          </Link>
+        </div>
+        <div id="logout-btn-hello">
+          <img src={logout} width="85" height="28" onClick={logOut} />
+        </div>
       </div>
     </>
   );
