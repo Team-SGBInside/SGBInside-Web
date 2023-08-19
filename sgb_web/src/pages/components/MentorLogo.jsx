7d217de0b8d8952c77b9cb988ec901ae2a42f619
biiit4894 <ghiesf@naver.com>
2023-08-20 05:08:56 +0900

    [FIX] 멘토 로그인 여부에 따라 로고 클릭시 로그인 홈 / 미로그인 홈으로 이동하도록 분기처리

--- sgb_web/src/pages/components/MentorLogo.jsx
+++ sgb_web/src/pages/components/MentorLogo.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import sgb_logo_mentor from './img/sgb_logo_mentor.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import sgb_logo_mentor from "./img/sgb_logo_mentor.png";
+import { Link } from "react-router-dom";
+import { getCookie, setCookie, removeCookie } from "../../lib/cookie";
 
 const MentorLogo = () => {
+  const userId = getCookie("userId");
+  const token = getCookie("accessToken");
+
+  // 미로그인 시 로고 클릭 - 미로그인 멘토 홈으로 이동
+  if (!userId || !token) {
     return (
-        <Link to = '/mentorHome'>
-        <div class = "logo">
-            <div> 
-                <img src={sgb_logo_mentor} alt="logo" width="264.5" height="87"/>
-            </div>
+      <Link to="/mentorHome">
+        <div class="logo">
+          <div>
+            <img src={sgb_logo_mentor} alt="logo" width="264.5" height="87" />
+          </div>
         </div>
-        </Link>
+      </Link>
     );
+  } else {
+    // 로그인 시 로고 클릭 - 로그인 멘토 홈으로 이동
+    return (
+      <Link to="/loginedMentorHome">
+        <div class="logo">
+          <div>
+            <img src={sgb_logo_mentor} alt="logo" width="264.5" height="87" />
+          </div>
+        </div>
+      </Link>
+    );
+  }
 };
 
 export default MentorLogo;
