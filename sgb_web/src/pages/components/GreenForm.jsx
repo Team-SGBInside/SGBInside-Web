f5a5f570f7456f2af47b23ed2633a1b8984a9907
biiit4894 <ghiesf@naver.com>
2023-09-12 00:01:54 +0900

    [DEL] 창체 기록 request body에서 작성자id 삭제

--- sgb_web/src/pages/components/GreenForm.jsx
+++ sgb_web/src/pages/components/GreenForm.jsx
@@ -31,7 +31,6 @@ function GreenForm() {
           semester: greenSemesterRef.current.value,
           role: greenActivityRef.current.value,
           thoughts: greenThoughtRef.current.value,
-          writerId: 9,
         },
         {
           headers: {
@@ -42,7 +41,9 @@ function GreenForm() {
         }
       );
       console.log(response);
-      window.alert("성공적으로 기록되었습니다. 기록한 내용은 '마이페이지'에서 확인 가능합니다."); //성공페이지로 라우팅
+      window.alert(
+        "성공적으로 기록되었습니다. 기록한 내용은 '마이페이지'에서 확인 가능합니다."
+      ); //성공페이지로 라우팅
     } catch (error) {
       console.log(error);
       window.alert("기록에 실패했습니다. 필수입력란을 전부 기입해주세요."); //실패페이지로 라우팅
