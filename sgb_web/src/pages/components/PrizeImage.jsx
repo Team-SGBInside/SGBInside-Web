0c8143ef81a9ebd5b1712bfb0a02b717cd614f65
biiit4894 <ghiesf@naver.com>
2023-09-11 14:24:35 +0900

    [ADD] 상장 이미지 input 컴포넌트화

---
+++ sgb_web/src/pages/components/PrizeImage.jsx
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import "./PrizeImage.css";
+const PrizeImage = ({ src, parentFunction }) => {
+  const [image, setImage] = useState(src);
+  const onChange = (e) => {
+    setImage(e.target.files[0]);
+    parentFunction(e.target.files[0]);
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+  return (
+    <>
+      <div id="image-input-label">상장 이미지 수정</div>
+      <img src={image}></img>
+      <br />
+      <input
+        type="file"
+        accept="image/*"
+        name="profile_img"
+        onChange={onChange}
+      />
+    </>
+  );
+};
+
+export default PrizeImage;
